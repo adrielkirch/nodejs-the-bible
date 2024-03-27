fb54dde5f1bb9f1a6784615c8f9a033b2c53e81b
Adriel Kirch <adriel.kirch.1@gmail.com>
2024-03-27 13:50:06 -0300

    tested soa

---
+++ 1_javascript/8_databases/2_lesson_mysql/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  PORT: process.env.PORT,
+  SALT: process.env.SALT,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+  MYSQL_HOST: process.env.MYSQL_HOST,
+  MYSQL_USER: process.env.MYSQL_USER,
+  MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+};
