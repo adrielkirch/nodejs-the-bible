f674f2f9711c5c108bd0d064689d2e19ef82ba0f
Adriel Kirch <adriel.kirch.1@gmail.com>
2024-03-27 10:36:20 -0300

    feat: new project struct

--- 2_module_express/3_lesson_soa_architecture/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT,
-  SALT: process.env.SALT,
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-};
--- 8_databases/1_lesson_mongodb/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT,
-  SALT: process.env.SALT,
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-  MONGO_URI: process.env.MONGO_URI,
-};
