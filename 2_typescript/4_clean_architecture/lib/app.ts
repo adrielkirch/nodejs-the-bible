c9aae67373b7a55733154cf2bb4c4d91d89a85a2
adrielkirch <adriel.kirch.1@gmail.com>
2024-04-10 16:26:00 -0300

    feat: npm package manager security tools finished

--- 2_typescript/4_clean_architecture/lib/app.ts
+++ 2_typescript/4_clean_architecture/lib/app.ts
@@ -13,12 +13,12 @@ import dependecyInjections from "./dependency-injection";
 import Container from "typedi";
 
 /**
- * Lesson Objective: Implementing a Service-Oriented Architecture (SOA), layered architecture with a fake JSON database to simulate authentication.
+ * Lesson Objective: Implementing a  layered architecture with a fake JSON database to simulate authentication.
  * With all necessary processes to guarantee security, organization, readability, maintainability and test-driven development.
  * This async server is built using Express.js, a web application framework for Node.js.
  * It provides a simple and minimalist web server that can handle HTTP requests.
  * @summary Express server for handling HTTP requests and responses.
- * @description This server aims to implement step by step for a Service-Oriented Architecture (SOA) with a fake JSON database for simulating authentication.
+ * @description This server aims to implement step by step for a Layered architecture with a fake JSON database for simulating authentication.
  * @since 1.0.0
  * @see {@link http://localhost:3000/api-docs API Documentation}
  * @returns {Promise<void>} A Promise that resolves when the server has started successfully.
